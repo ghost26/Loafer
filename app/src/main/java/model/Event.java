a1faf3aee1ac3c3579c2a3f22cc9481f260e78a9
ghost26 <russtav262@yandex.ru>
2015-12-06 02:30:21 +0300

    Event changed, Location added

--- app/src/main/java/model/Event.java
+++ app/src/main/java/model/Event.java
@@ -6,36 +6,92 @@ import java.net.URI;
  * Created by ruslanabdulhalikov on 05.12.15.
  */
 public class Event {
-    public final int id;
-    public final String created_at;
-    public final String starts_at;
-    public final String ends_at;
-    public final String name;
-    public final String description_short;
-    public final URI default_url;
-    public final URI url;
-    public final String country;
-    public final String city;
-    public final String address;
-    public final double latitude;
-    public final double longitude;
+    Location location;
+    private int id;
+    private String createdAt;
+    private String startAt;
+    private String endsAt;
+    private String name;
+    private String descriptionShort;
+    private URI defaultImageUrl;
+    private URI eventUrl;
 
-    public Event(int id, String created_at, String starts_at, String ends_at, String name,
-                 String description_short, URI default_url, URI url,
-                 String country, String city, String address,
-                 double latitude, double longitude) {
+    public Event(int id, String createdAt, String startAt, String endsAt, String name,
+                 String descriptionShort, URI default_url, URI eventUrl,
+                 Location location) {
         this.id = id;
-        this.created_at = created_at;
-        this.starts_at = starts_at;
-        this.ends_at = ends_at;
+        this.createdAt = createdAt;
+        this.startAt = startAt;
+        this.endsAt = endsAt;
         this.name = name;
-        this.description_short = description_short;
-        this.default_url = default_url;
-        this.url = url;
-        this.country = country;
-        this.city = city;
-        this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
+        this.descriptionShort = descriptionShort;
+        this.defaultImageUrl = default_url;
+        this.eventUrl = eventUrl;
+        this.location = location;
     }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(String startAt) {
+        this.startAt = startAt;
+    }
+
+    public String getEndsAt() {
+        return endsAt;
+    }
+
+    public void setEndsAt(String endsAt) {
+        this.endsAt = endsAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptionShort() {
+        return descriptionShort;
+    }
+
+    public void setDescriptionShort(String descriptionShort) {
+        this.descriptionShort = descriptionShort;
+    }
+
+    public URI getDefaultImageUrl() {
+        return defaultImageUrl;
+    }
+
+    public void setDefaultImageUrl(URI defaultImageUrl) {
+        this.defaultImageUrl = defaultImageUrl;
+    }
+
+    public URI getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(URI eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
 }
