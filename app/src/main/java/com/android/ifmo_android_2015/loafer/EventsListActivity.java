bc44ba75005dd6c180888eaa4dd434883031d55a
daniil <voidmaster666@gmail.com>
2015-12-11 20:23:20 +0300

    Add detailed information for item of recyclerview

--- app/src/main/java/com/android/ifmo_android_2015/loafer/EventsListActivity.java
+++ app/src/main/java/com/android/ifmo_android_2015/loafer/EventsListActivity.java
@@ -1,11 +1,13 @@
 package com.android.ifmo_android_2015.loafer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EventsListActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -39,4 +41,14 @@ public class EventsListActivity extends AppCompatActivity {
         return mDataSet;
     }
 
+    public void onClick(View view) {
+        TextView name = (TextView) view.findViewById(R.id.recycler_item_name);
+        TextView address = (TextView) view.findViewById(R.id.recycler_item_address);
+        ImageView image = (ImageView) view.findViewById(R.id.recycler_item_image);
+
+        Intent detailedInfo = new Intent(this, DetailedInfoActivity.class);
+        detailedInfo.putExtra("NAME", name.getText().toString());
+        detailedInfo.putExtra("ADDRESS", address.getText().toString());
+        startActivity(detailedInfo);
+    }
 }
